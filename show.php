1553fceae2c9db19dc4791d13164b1591eacd223
justojaramillo <justojaramillo1991@gmail.com>
2024-12-26 22:24:05 -0500

    show comments in construction

--- show.php
+++ show.php
@@ -10,6 +10,10 @@ if (isset($_GET["id"])) {
     $post = $one_post->fetch(PDO::FETCH_OBJ);
 }
 
+$comments = $conn->query("SELECT * FROM comments WHERE post_id='$id'");
+$comments->execute();
+$comment = $comments->fetchAll(PDO::FETCH_OBJ);
+
 ?>
 <main class="form-signin w-90 m-auto mt-5">
     <div class="row">
@@ -25,22 +29,34 @@ if (isset($_GET["id"])) {
     <div class="row ">
         <form method="POST" id="comment_data">
     
-        <div class="form-floating mt-3">
-          <input value="<?= $post->post_id ?>" name="post_id" type="hidden" class="form-control" id="post_id">
-        </div>
-    
-        <div class="form-floating mt-3">
-          <input value="<?= $_SESSION["username"] ?>" name="username" type="hidden" class="form-control" id="username">
-        </div>    
+            <div class="form-floating mt-3">
+            <input value="<?= $post->post_id ?>" name="post_id" type="hidden" class="form-control" id="post_id">
+            </div>
+        
+            <div class="form-floating mt-3">
+            <input value="<?= $_SESSION["username"] ?>" name="username" type="hidden" class="form-control" id="username">
+            </div>    
 
-        <div class="form-floating mt-3">
-          <textarea rows="9" name="comment" placeholder="comment" class="form-control" id="comment"></textarea>
-          <label for="floatingPassword">Comment</label>
-        </div>
-    
-        <button name="submit" id="submit" class="w-100 btn btn-lg btn-primary mt-5" type="submit">Create comment</button>
+            <div class="form-floating mt-3">
+            <textarea rows="9" name="comment" placeholder="comment" class="form-control" id="comment"></textarea>
+            <label for="floatingPassword">Comment</label>
+            </div>
+        
+            <button name="submit" id="submit" class="w-100 btn btn-lg btn-primary mt-5" type="submit">Create comment</button>
+        <div id="msg" class="nothing"></div>
     
       </form>
+    </div>
+    <div class="row">
+        <?php foreach ($comment as $comm) : ?>
+        <div class="card mt-5">
+            <div class="card-body">
+                <p class="card-text"><?= $comm->username ?></p>
+                <h5 class="card-title"><?= $comm->comment ?> </h5>
+            </div>
+        </div>
+        <?php endforeach; ?>
+        <!-- AQUI QUEDE MINUTO 6:52 -->
     </div>
 </main>
 
@@ -48,7 +64,7 @@ if (isset($_GET["id"])) {
 <?php require_once "includes/footer.php"; ?>
 
 <script>
-    $( document ).ready(function(e) {
+    $( document ).ready(function() {
         
         
 
@@ -63,7 +79,10 @@ if (isset($_GET["id"])) {
                 url: 'http://commentsys.test/insert_comment.php',
                 data: form_data,
                 success: function (){
-                    alert('success');
+                    $('#comment').val('');
+                    $('#username').val('');
+                    $('#post_id').val('');
+                    $("#msg").html("Added successfully.").toggleClass("alert alert-success bg-success text-white mt-3");
                 }
             });
         });
